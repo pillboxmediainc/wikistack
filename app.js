44611f737abef23a56a8d6b949cc90e9b0a78395
Barry Huang <Barry.huang93@gmail.com>
2018-11-09 12:40:32 -0500

    section3 done

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-const Sequelize = require('sequelize');
-// const db = new Sequelize('postgress://localhost:5432/you-db');
-
 const express = require('express');
 const routes = require('./routes/index');
 const app = express();
-
+const {db} = require('./models');
 app.use(express.static('./stylesheets'));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+});
+
+
+
+
+
+
+
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+const dbSync = async () => {
+  
+  try{
+    await db.sync({force:true});
+  } catch (err){
+    console.error(err);
+  }
+  
+  
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+}
+
+dbSync()
+
+
+// console.log('what is in db:' , db.models.page);
+
